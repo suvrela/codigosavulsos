901e17bc89d684efbee8e297cd3e4bbf719cccc4
RaylanSA <santosavilar.raylan@gmail.com>
2023-08-23 10:49:48 -0400

    new codes

---
+++ ex001.js
@@ -0,0 +1,7 @@
+var vel= 50
+console.log(`A velocidade do seu carro é ${vel}Km/h! `)
+if(vel > 60) {  //condição simples!
+  console.log('Você ultrapassou o limite de velocidade. MULTADO!! ')
+}
+console.log(`Dirija sempre usando cinto de segurança!! `)
+
